d5513cb8dd5271da2129fd92efc7721bec7baf05
setayeshghamat <145782254+setayeshghamat@users.noreply.github.com>
2024-03-11 11:38:07 +0100

    Update option added.

--- lib/pages/board.dart
+++ lib/pages/board.dart
@@ -15,6 +15,9 @@ class _BoardPageState extends State<BoardPage> {
   final TextEditingController _listNameController = TextEditingController();
   final TextEditingController _cardTitleController = TextEditingController();
   final TextEditingController _cardDescriptionController = TextEditingController();
+  final TextEditingController _editListNameController = TextEditingController();
+  final TextEditingController _editCardTitleController = TextEditingController();
+  final TextEditingController _editCardDescriptionController = TextEditingController();
   List<ListData> _lists = []; // A list to store multiple lists with cards
 
   @override
@@ -22,6 +25,9 @@ class _BoardPageState extends State<BoardPage> {
     _listNameController.dispose();
     _cardTitleController.dispose();
     _cardDescriptionController.dispose();
+    _editListNameController.dispose();
+    _editCardTitleController.dispose();
+    _editCardDescriptionController.dispose();
     super.dispose();
   }
 
@@ -103,6 +109,82 @@ class _BoardPageState extends State<BoardPage> {
       },
     );
   }
+  void _showEditListDialog(int listIndex) {
+    _editListNameController.text = _lists[listIndex].name;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Edit list name'),
+          content: TextField(
+            controller: _editListNameController,
+            decoration: const InputDecoration(hintText: 'New name of list'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: const Text('Update'),
+              onPressed: () {
+                if (_editListNameController.text.isNotEmpty) {
+                  setState(() {
+                    _lists[listIndex].name = _editListNameController.text;
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showEditCardDialog(int listIndex, int cardIndex) {
+    _editCardTitleController.text = _lists[listIndex].cards[cardIndex].title;
+    _editCardDescriptionController.text = _lists[listIndex].cards[cardIndex].description;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Edit card'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                controller: _editCardTitleController,
+                decoration: const InputDecoration(hintText: 'Title'),
+              ),
+              TextField(
+                controller: _editCardDescriptionController,
+                decoration: const InputDecoration(hintText: 'Description'),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: const Text('Update'),
+              onPressed: () {
+                if (_editCardTitleController.text.isNotEmpty && _editCardDescriptionController.text.isNotEmpty) {
+                  setState(() {
+                    _lists[listIndex].cards[cardIndex].title = _editCardTitleController.text;
+                    _lists[listIndex].cards[cardIndex].description = _editCardDescriptionController.text;
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   void _showCardDetailsDialog(int listIndex, int cardIndex) {
     CardData card = _lists[listIndex].cards[cardIndex];
@@ -139,38 +221,63 @@ class _BoardPageState extends State<BoardPage> {
       },
     );
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.name),
-      ),
-      body: ListView.builder(
-        itemCount: _lists.length,
-        itemBuilder: (context, listIndex) {
-          var listData = _lists[listIndex];
-          return ExpansionTile(
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text(widget.name),
+    ),
+    body: ListView.builder(
+      itemCount: _lists.length,
+      itemBuilder: (context, listIndex) {
+        var listData = _lists[listIndex];
+        return Card(
+          child: ExpansionTile(
             title: Text(listData.name),
-            children: List.generate(listData.cards.length, (cardIndex) {
-              CardData card = listData.cards[cardIndex];
-              return ListTile(
-                title: Text(card.title),
-                subtitle: Text(card.description),
-                onTap: () => _showCardDetailsDialog(listIndex, cardIndex),
-              );
-            }),
+            children: <Widget>[
+              for (var card in listData.cards)
+                ListTile(
+                  title: Text(card.title),
+                  subtitle: Text(card.description),
+                  onTap: () => _showCardDetailsDialog(listIndex, listData.cards.indexOf(card)),
+                  trailing: Wrap(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () => _showEditCardDialog(listIndex, listData.cards.indexOf(card)),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          setState(() {
+                            _lists[listIndex].cards.removeAt(listData.cards.indexOf(card));
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ListTile(
+                title: TextButton.icon(
+                  icon: Icon(Icons.add),
+                  label: Text('Add Card'),
+                  onPressed: () => _showAddCardDialog(listIndex),
+                ),
+              )
+            ],
             trailing: IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () => _showAddCardDialog(listIndex),
+              icon: Icon(Icons.edit),
+              onPressed: () => _showEditListDialog(listIndex),
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showCreateListDialog,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+          ),
+        );
+      },
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: _showCreateListDialog,
+      child: Icon(Icons.add),
+    ),
+  );
+}
+
 }
